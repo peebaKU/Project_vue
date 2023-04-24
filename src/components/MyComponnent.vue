bdcd55e3aeb017f1b9fa68aa8c1e32126d283842
Peeba <baramee.254450@gmail.com>
2023-04-24 16:36:13 +0700

    create Componnent

---
+++ src/components/MyComponnent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="content">
+        <h1>My Component</h1>
+        <p>my component {{myPropertyName}}</p>
+    </div>
+</template>
+
+
+<script>
+
+export default {
+    
+name: 'MyComponnent',
+data() {
+  return {
+    myPropertyName:"BARAMEE"
+  }  
+},       
+
+}
+</script>
+
+
+
+<style scoped>
+
+</style>
