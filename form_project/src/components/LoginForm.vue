4e131029783608f3b815ed2484a962131a8cbad7
Peeba <baramee.254450@gmail.com>
2023-04-27 04:49:05 +0700

    update

---
+++ form_project/src/components/LoginForm.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+        <div class="row mt-3">
+            <div class="row sm-4">
+                
+                <form>
+                    <img alt="Vue logo" src="../assets/logo.png" />
+                    <div class="form-group">
+                        <label for="username">User Name</label>
+                        <input type="text" name="username" id="username" placeholder="Enter your username" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" name="password" id="password" placeholder="Enter your password" class="form-control">
+                    </div>
+                    <button type="submit" class="btn btn-primary my-1">Login</button>
+                </form>
+            </div>
+        </div>
+    </div>    
+</template>
+
+<script>
+
+export default{
+    name: 'LoginForm',
+    data(){
+        return {}       
+    },
+}
+</script>
+
+
