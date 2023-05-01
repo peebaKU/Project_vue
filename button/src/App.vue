0e0c661688607087a604154e77c7bc528feb7eb3
Peeba <baramee.254450@gmail.com>
2023-05-02 00:23:15 +0700

    About crating button in vue

---
+++ button/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <CountBtn />
+</template>
+
+<script>
+import CountBtn from './components/CountBtn.vue'
+
+export default  {
+  name: 'App',
+  components: {
+    CountBtn,
+  }
+}
+</script>
+
+<style>
+ 
+</style>
